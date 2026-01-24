d6d78cb2b2704584334ed70795dd8a15541180b2
coolcrybaby <findnat.here@gmail.com>
2026-01-24 13:32:46 +0800

    Delete S10272645_PRG2Assignment directory
    Deleting old stuff to add new code

--- S10272645_PRG2Assignment/S10272645_PRG2Assignment/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//==========================================================
-// Student Number : S10272645
-// Student Name : Alexander Wong
-// Partner Name : Nathan Ho
-//==========================================================
