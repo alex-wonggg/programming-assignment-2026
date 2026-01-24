36bc14696aac8424f815ebbed9b4fb71bb707369
coolcrybaby <findnat.here@gmail.com>
2026-01-24 18:32:46 +0800

    deleted files
    directory name too long

--- programming-assignment-2026-main/programming-assignment-2026-main/S10272645_PRG2Assignment/S10272645_PRG2Assignment/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//==========================================================
-// Student Number : S10272082
-// Student Name : Nathan Ho
-// Partner Name : Alexander Wong
-//==========================================================
