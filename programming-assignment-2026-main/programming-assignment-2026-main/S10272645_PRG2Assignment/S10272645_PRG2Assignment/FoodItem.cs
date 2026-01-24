a843896c5d7ccd0ec2978ae8fc7d483551dbdcd4
coolcrybaby <findnat.here@gmail.com>
2026-01-24 13:37:11 +0800

    uploaded edits to proj

---
+++ programming-assignment-2026-main/programming-assignment-2026-main/S10272645_PRG2Assignment/S10272645_PRG2Assignment/FoodItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//==========================================================
+// Student Number : S10272082
+// Student Name : Nathan Ho
+// Partner Name : Alexander Wong
+//==========================================================
+
+namespace S10272645_PRG2Assignment
+{
+    internal class FoodItem
+    {
+        private string itemName;
+        private string itemDesc;
+        private double itemPrice;
+        private string customise;
+
+        public string ItemName { get { return itemName; } set { itemName = value; } }
+        public string ItemDesc { get { return itemDesc; } set { itemDesc = value; } }
+        public double ItemPrice { get { return itemPrice; } set { itemPrice = value; } }
+        public string Customise { get { return customise; } set { customise = value; } }
+
+        public string ToString()
+        {
+            return "joe";
+        }
+
+        public FoodItem() { }
+        public FoodItem(string n, string d, double p, string c)
+        {
+            ItemName = n;
+            ItemDesc = d;
+            ItemPrice = p;
+            Customise = c;
+        }
+    }
+}
---
+++ programming-assignment-2026-main/programming-assignment-2026-main/S10272645_PRG2Assignment/S10272645_PRG2Assignment/Menu.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//==========================================================
+// Student Number : S10272082
+// Student Name : Nathan Ho
+// Partner Name : Alexander Wong
+//==========================================================
+
+namespace S10272645_PRG2Assignment
+{
+    internal class Menu
+    {
+        private string menuId;
+        private string menuName;
+
+        public string MenuId { get { return menuId; } set { menuId = value; } }
+        public string MenuName { get { return menuName; } set {menuName = value; } }
+
+        public List<FoodItem> MenuItems { get; set; } = new List<FoodItem>;
+        public void AddFoodItem(FoodItem fooditem)
+        {
+            MenuItems.Add(fooditem);
+        }
+        public bool RemoveFoodItem(FoodItem fooditem)
+        {
+            try
+            {
+                MenuItems.Remove(fooditem);
+                return true;
+            }
+            catch { return false; }
+        }
+        public void DisplayFoodItems()
+        {
+            foreach (FoodItem item in MenuItems)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+
+        public string ToString()
+        {
+            return "joe";
+        }
+        public Menu() { }
+        public Menu(string I, string n)
+        {
+            this.menuId = I;
+            this.menuName = n;
+        }
+
+    }
+}
